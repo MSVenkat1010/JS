45bf80d2f22ae6de1027323bbfbaa02e74ed93b7
MSVenkat1010 <kmanjusai.venkat@gmail.com>
2025-07-13 15:02:45 +0530

    Add initial Helloworld.js file with basic console log example

---
+++ Helloworld.js
@@ -0,0 +1,7 @@
+console.log("Hello, World!");
+// This is a simple JavaScript program that prints "Hello, World!" to the console.
+// You can run this code in any JavaScript environment, such as a web browser or Node.js.
+// To execute this code, simply copy and paste it into your JavaScript console or save it
+// in a file named Helloworld.js and run it using Node.js with the command:
+// node Helloworld.js
+// Enjoy coding!
